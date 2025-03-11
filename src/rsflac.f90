da7ed0e8945012bf321af1eecce76035c7c05aa3
Chase J. Shyu <iamhemry@gmail.com>
2025-03-11 11:46:56 -0500

    fix bug in restart

--- src/rsflac.f90
+++ src/rsflac.f90
@@ -34,8 +34,8 @@ read (1,rec=nrec) cord
 close (1)
 
 ! min element width and thickness
-dxmin = minval(cord(1,2:nx,1) - cord(1,1:nx-1,1))
-dzmin = minval(cord(2,1,1:nz-1) - cord(2,1,2:nz))
+dxmin = minval(cord(2:nx,1,1) - cord(1:nx-1,1,1))
+dzmin = minval(cord(1,1:nz-1,2) - cord(1,2:nz,2))
 
 open (1,file='dhacc.rs',access='direct',recl=(nx-1)*kindr)
 read (1,rec=nrec) dhacc(1:nx-1)
